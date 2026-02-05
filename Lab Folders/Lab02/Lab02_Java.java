88e24074c751c7281dd9cfbcc4b21d49f3f6d673
m-khan-y31 <m.khanyaqoob3281@gmail.com>
2026-02-05 10:08:29 +0000

    Update Lab02_Java.java
    Changes made by student

--- Lab Folders/Lab02/Lab02_Java.java
+++ Lab Folders/Lab02/Lab02_Java.java
@@ -2,3 +2,26 @@
 // Sample Java file
 // Edit by instructor (khanAston)
 // test line cspaces
+
+public class Lab02 {
+
+    // function equivalent to Haskell inc
+    public static int inc(int x) {
+        return x + 1;
+    }
+
+    // function that prints 1 + 1 = 2
+    public static void onePlusOne() {
+        System.out.println("1 + 1 = 2");
+    }
+
+    public static void main(String[] args) {
+
+        // call increment function
+        int result = inc(5);
+        System.out.println("inc(5) = " + result);
+
+        // call printing function
+        onePlusOne();
+    }
+}
